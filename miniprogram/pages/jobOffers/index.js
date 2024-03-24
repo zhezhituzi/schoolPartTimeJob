f6625d394061f9f58a2d111931656b04866601da
cao.guangxu <zhezhituzi@gmail.com>
2024-03-24 08:10:03 +0800

    fix bug

--- miniprogram/pages/jobOffers/index.js
+++ miniprogram/pages/jobOffers/index.js
@@ -16,11 +16,13 @@ Page({
       success: function(res) {
         const newJobs = res.result.data; // 假设云函数返回的数据在 res.result.data 中
         const newLastId = res.result.lastId; // 获取返回的最新的lastId
-        console.log('获取到的岗位数据:', newJobs);
-        that.setData({
-          jobs: that.data.jobs.concat(newJobs), // 将新获取到的岗位数据追加到现有数据后面
-          lastId: newLastId // 更新lastId，用于下次分页加载
-        });
+        console.log('从云端获取到的岗位数据:', newJobs);
+        if(res.result.data.length >0 ){
+          that.setData({
+            jobs: that.data.jobs.concat(newJobs), // 将新获取到的岗位数据追加到现有数据后面
+            lastId: newLastId // 更新lastId，用于下次分页加载
+          });
+        }
       },
       fail: function(err) {
         console.error('获取岗位数据失败:', err);
